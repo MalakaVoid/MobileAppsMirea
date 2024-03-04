d490cd869a17a4812996a2eaaeb52aac8bba25dc
Daniil <azbukin2003@gmail.com>
2024-03-04 20:58:11 +0300

    Lesson 8 without controll lesson

---
+++ Lesson8/yandexdriver/src/main/java/ru/mirea/azbukindu/yandexdriver/MainActivity.java
@@ -0,0 +1,220 @@
+package ru.mirea.azbukindu.yandexdriver;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.PointF;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKit;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.RequestPoint;
+import com.yandex.mapkit.RequestPointType;
+import com.yandex.mapkit.directions.DirectionsFactory;
+import com.yandex.mapkit.directions.driving.DrivingOptions;
+import com.yandex.mapkit.directions.driving.DrivingRoute;
+import com.yandex.mapkit.directions.driving.DrivingRouter;
+import com.yandex.mapkit.directions.driving.DrivingSession;
+import com.yandex.mapkit.directions.driving.VehicleOptions;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.layers.ObjectEvent;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.CompositeIcon;
+import com.yandex.mapkit.map.IconStyle;
+import com.yandex.mapkit.map.MapObject;
+import com.yandex.mapkit.map.MapObjectCollection;
+import com.yandex.mapkit.map.MapObjectTapListener;
+import com.yandex.mapkit.map.PlacemarkMapObject;
+import com.yandex.mapkit.map.RotationType;
+import com.yandex.mapkit.mapview.MapView;
+import com.yandex.mapkit.user_location.UserLocationLayer;
+import com.yandex.mapkit.user_location.UserLocationObjectListener;
+import com.yandex.mapkit.user_location.UserLocationView;
+import com.yandex.runtime.Error;
+import com.yandex.runtime.image.ImageProvider;
+import com.yandex.runtime.network.NetworkError;
+import com.yandex.runtime.network.RemoteError;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ru.mirea.azbukindu.yandexdriver.databinding.ActivityMainBinding;
+public class MainActivity extends AppCompatActivity implements DrivingSession.DrivingRouteListener, UserLocationObjectListener {
+    private ActivityMainBinding binding;
+    private final String MAPKIT_API_KEY = "376d5852-2c53-4db8-ac72-61d8e64ec649";
+    private final Point ROUTE_START_LOCATION = new Point(55.6028, 37.4156);
+    private Point ROUTE_START_LOC;
+    private final Point ROUTE_END_LOCATION = new Point(55.7284, 36.8619);
+//    private final Point SCREEN_CENTER = new Point(
+//            (ROUTE_START_LOCATION.getLatitude() + ROUTE_END_LOCATION.getLatitude()) / 2,
+//            (ROUTE_START_LOCATION.getLongitude() + ROUTE_END_LOCATION.getLongitude()) /
+//                    2);
+    private MapView mapView;
+    private MapObjectCollection mapObjects;
+    private DrivingRouter drivingRouter;
+    private UserLocationLayer userLocationLayer;
+    private boolean isWork;
+    private DrivingSession drivingSession;
+    private int[] colors = {0xFFFF0000, 0xFF00FF00, 0x00FFBBBB, 0xFF0000FF};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MapKitFactory.initialize(this);
+        DirectionsFactory.initialize(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        mapView = binding.mapview;
+        mapView.getMap().setRotateGesturesEnabled(false);
+        // Устанавливаем начальную точку и масштаб
+        mapView.getMap().move(new CameraPosition(
+                ROUTE_START_LOCATION, 10, 0, 0));
+        // Ининциализируем объект для создания маршрута водителя
+        drivingRouter = DirectionsFactory.getInstance().createDrivingRouter();
+        mapObjects = mapView.getMap().getMapObjects().addCollection();
+
+        int  locationPermissionStatus = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (locationPermissionStatus == PackageManager.PERMISSION_GRANTED)  {
+            isWork = true;
+        } else {
+            ActivityCompat.requestPermissions(this,  new  String[] {
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+            },  100);
+        }
+
+        mapView.getMap().getMapObjects().addPlacemark(ROUTE_END_LOCATION).addTapListener(new MapObjectTapListener() {
+            @Override
+            public boolean onMapObjectTap(@NonNull MapObject mapObject, @NonNull Point
+                    point) {
+                Toast.makeText(getApplication(),"ВКУСНО И ТОЧКА ОПИСАНИЕ",
+                        Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        if (isWork){
+            loadUserLocationLayer();
+        }
+    }
+    private void loadUserLocationLayer(){
+        MapKit mapKit = MapKitFactory.getInstance();
+        mapKit.resetLocationManagerToDefault();
+        userLocationLayer = mapKit.createUserLocationLayer(mapView.getMapWindow());
+        userLocationLayer.setVisible(true);
+        userLocationLayer.setHeadingEnabled(true);
+        userLocationLayer.setObjectListener(this);
+    }
+    @Override
+    protected void onStop() {
+        mapView.onStop();
+        MapKitFactory.getInstance().onStop();
+        super.onStop();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        MapKitFactory.getInstance().onStart();
+        mapView.onStart();
+    }
+
+    private void submitRequest(Point startLoc) {
+        DrivingOptions drivingOptions = new DrivingOptions();
+        VehicleOptions vehicleOptions = new VehicleOptions();
+        // Кол-во альтернативных путей
+        drivingOptions.setRoutesCount(4);
+        ArrayList<RequestPoint> requestPoints = new ArrayList<>();
+        // Устанавка точек маршрута
+        requestPoints.add(new RequestPoint(startLoc,
+                RequestPointType.WAYPOINT,
+                null));
+        requestPoints.add(new RequestPoint(ROUTE_END_LOCATION,
+                RequestPointType.WAYPOINT,
+                null));
+        // Отправка запроса на сервер
+        drivingSession = drivingRouter.requestRoutes(requestPoints, drivingOptions,
+                vehicleOptions, this);
+    }
+
+    @Override
+    public void onDrivingRoutes(@NonNull List<DrivingRoute> list) {
+        int color;
+        for (int i = 0; i < list.size(); i++) {
+            // настроиваем цвета для каждого маршрута
+            color = colors[i];
+            // добавляем маршрут на карту
+            mapObjects.addPolyline(list.get(i).getGeometry()).setStrokeColor(color);
+        }
+    }
+
+    @Override
+    public void onDrivingRoutesError(@NonNull Error error) {
+        String errorMessage = "ERROR";
+        if (error instanceof RemoteError) {
+            errorMessage = "ERROR";
+        } else if (error instanceof NetworkError) {
+            errorMessage = "ERROR";
+        }
+        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onObjectRemoved(@NonNull UserLocationView userLocationView) {
+    }
+    @Override
+    public void onObjectUpdated(@NonNull UserLocationView userLocationView,
+                                @NonNull ObjectEvent objectEvent) {
+    }
+
+    @Override
+    public void onObjectAdded(UserLocationView userLocationView) {
+        userLocationLayer.setAnchor(
+                new PointF((float)(mapView.getWidth() * 0.5),
+                        (float)(mapView.getHeight() * 0.5)),
+                new PointF((float)(mapView.getWidth() * 0.5),
+                        (float)(mapView.getHeight() * 0.83)));
+        // При определении направления движения устанавливается следующая иконка
+        userLocationView.getArrow().setIcon(ImageProvider.fromResource(
+                this, android.R.drawable.arrow_up_float));
+
+        // При получении координат местоположения устанавливается следующая иконка
+        CompositeIcon pinIcon = userLocationView.getPin().useCompositeIcon();
+        pinIcon.setIcon(
+                "pin",
+                ImageProvider.fromResource(this, R.drawable.ic_launcher_background),
+                new IconStyle().setAnchor(new PointF(0.5f, 0.5f))
+                        .setRotationType(RotationType.ROTATE)
+                        .setZIndex(1f)
+                        .setScale(0.5f)
+        );
+
+        userLocationView.getAccuracyCircle().setFillColor(Color.BLUE & 0x99ffffff);
+
+        FusedLocationProviderClient fusedLocation = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocation.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                Location location = task.getResult();
+                if (location == null) {
+                    Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    submitRequest(new Point(location.getLatitude(), location.getLongitude()));
+                }
+            }
+        });
+
+    }
+
+}
---
+++ Lesson8/yandexmaps/src/main/java/ru/mirea/azbukindu/yandexmaps/App.java
@@ -0,0 +1,15 @@
+package ru.mirea.azbukindu.yandexmaps;
+
+import android.app.Application;
+
+import com.yandex.mapkit.MapKitFactory;
+
+public class App extends Application {
+    private final String MAPKIT_API_KEY = "376d5852-2c53-4db8-ac72-61d8e64ec649";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Set the api key before calling initialize on MapKitFactory.
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+    }
+}
